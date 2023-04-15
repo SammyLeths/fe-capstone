d222b494252890fff9f5e1cb41bae49287354b17
SammyLeths <s.o.eyiowuawi@gmail.com>
2023-04-15 18:13:20 +0100

    complete home page sections

--- src/layouts/footer/Footer.jsx
+++ src/layouts/footer/Footer.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
+import styles from './footer.module.scss';
+import footer_logo from '../../assets/img/footer-logo.png';
 
 const Footer = () => {
   return (
-    <footer>
-      <div>
-        <h1>This is the footer</h1>
+    <footer className={styles.section}>
+      <div className={styles.container}>
+        <div className={styles.logo}>
+          <img src={footer_logo} alt='Little Lemon Footer Logo' />
+        </div>
+        <div className={styles.navigation}>
+          <h3>Navigation</h3>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Menu</li>
+            <li>Reservations</li>
+            <li>Order Online</li>
+            <li>Login</li>
+          </ul>
+        </div>
+        <div className={styles.contact}>
+          <h3>Admin</h3>
+          <ul>
+            <li>Phone Number</li>
+            <li>Email</li>
+          </ul>
+        </div>
+        <div className={styles.socials}>
+          <ul>
+            <h3>Socials</h3>
+            <li>Facebook</li>
+            <li>Instagram</li>
+            <li>Linkedin</li>
+            <li>Twitter</li>
+          </ul>
+        </div>
       </div>
     </footer>
   );
--- src/pages/home/about/About.jsx
+++ src/pages/home/about/About.jsx
@@ -1,8 +1,36 @@
 import React from 'react';
-//import styles from './about.module.scss';
+import styles from './about.module.scss';
+import chef_a from '../../../assets/img/chef-a.jpg';
+import chef_b from '../../../assets/img/chef-b.jpg';
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section className={styles.section}>
+      <div className={styles.container}>
+        <div className={styles.copy}>
+          <div className={styles.heading}>
+            <h1>Little Lemon</h1>
+            <h2>Chicago</h2>
+          </div>
+          <div className={styles.text}>
+            <p>
+              Little Lemon is a charming neighbourhood bistro that serves simple
+              food and classic cocktails in a lively but casual environment. The
+              restaurant features a locally-sourced menu with daily specials.{' '}
+            </p>
+            <p>
+              We are a family owned Mediterranean restaurant, focused on
+              traditional recipes served with a modern twist.
+            </p>
+          </div>
+        </div>
+        <div className={styles.image}>
+          <img className={styles.chef_a} src={chef_b} alt='Chef Mario' />
+          <img className={styles.chef_b} src={chef_a} alt='Chef Adrian' />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
